2327fbf1a6b07af324d448bba2d26037d11768ae
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2023-08-02 01:14:00 -0300

    getter usage

--- quiz_app/lib/screens/results_screen.dart
+++ quiz_app/lib/screens/results_screen.dart
@@ -13,7 +13,7 @@ class ResultsScreen extends StatelessWidget {
     required this.onRestart,
   });
 
-  List<Map<String, Object>> getSummaryData() {
+  List<Map<String, Object>> get summaryData {
     final List<Map<String, Object>> summary = [];
 
     for (var i = 0; i < choosenAnswers.length; i++) {
@@ -30,7 +30,6 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final summaryData = getSummaryData();
     final numTotalQuestions = questions.length;
     final numCorrectQuestions = summaryData.where((data) => data['correct_answer'] == data['user_answers']).length;
 
